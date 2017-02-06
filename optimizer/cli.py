85d9c620abdc548a59e0ac9e8679dbeaf97ca36e
Patrick Cullen <patrick.cullen@washpost.com>
2017-02-06 14:35:27 -0500

    added list services

--- optimizer/cli.py
+++ optimizer/cli.py
@@ -2,15 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import click
+from pprint import pprint
+from ecs import ECS
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.option('--cluster', help='ECS cluster to optimize.')
+@click.argument('cluster')
 def service(cluster):
-    print cluster
+    ecs = ECS()
+    for service in ecs.list_services(cluster):
+        print service
 
 if __name__ == '__main__':
     cli()
