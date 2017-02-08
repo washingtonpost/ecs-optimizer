f9c55ecf0068e63c943a2345ba1b2ee9500b3d0e
Patrick Cullen <patrick.cullen@washpost.com>
2017-02-08 08:02:50 -0500

    refactored

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -1,34 +1,32 @@
 class MemoryOptimizer(object):
     def optimize(self, ecs, cloudwatch, cluster, service, start_date, end_date, over_reserve, under_reserve):
-        max_memory_utilization = cloudwatch.max_memory_utilization(cluster, service, start_date, end_date)
-        if max_memory_utilization <= 0.0:
+        utilization = cloudwatch.max_memory_utilization(cluster, service, start_date, end_date)
+        if utilization <= 0.0:
             return
 
         hard_limit, soft_limit = ecs.service_memory_reservation(cluster, service)
-        peak_memory, new_hard_limit, new_soft_limit = self.recommend(max_memory_utilization, hard_limit, soft_limit, under_reserve)
+        peak_memory = soft_limit*(utilization/100.0)
+        low_memory = peak_memory * (1.0 - under_reserve)
+        high_memory = peak_memory * (1.0 + over_reserve)
 
-        if new_soft_limit != soft_limit:
-            print "%s used %dMB out of %dMB reserved memory. Recommended new hard/soft limit of %dMB/%dMB." % \
-                (service, peak_memory, soft_limit, new_hard_limit, new_soft_limit)
+        if low_memory > soft_limit or high_memory < soft_limit:
+            new_hard_limit, new_soft_limit = self.recommend(low_memory)
+            # only make recommendation on new soft_limit because reserved memory is the only cost driver
+            if new_soft_limit != soft_limit:
+                print "%s used %dMB with hard/soft limit of %dMB/%dMB. Recommended new hard/soft limit of %dMB/%dMB." % \
+                    (service, peak_memory, hard_limit, soft_limit, new_hard_limit, new_soft_limit)
 
-    def recommend(self, utilization, hard_limit, soft_limit, under_reserve=0.1):
-        peak = soft_limit*(utilization/100.0)
-        new_hard_limit, new_soft_limit = self._calculate_memory_chunk(peak, under_reserve)
-
-        return int(peak), int(new_hard_limit), int(new_soft_limit)
-
-    def _calculate_memory_chunk(self, memory, under_reserve):
+    def recommend(self, memory):
         memory_chunk = 4 # 4MB is the min memory chunk
         prev_memory_chunk = memory_chunk
         soft_limit = 0
         hard_limit = 0
-        low_memory = memory * (1.0 - under_reserve)
 
-        while (memory_chunk * 4) < low_memory:
+        while (memory_chunk * 4) < memory:
             prev_memory_chunk = memory_chunk
             memory_chunk *= 2
 
-        while soft_limit < low_memory:
+        while soft_limit < memory:
             soft_limit += memory_chunk
 
         # hard limit is at least 25% greater than the soft limit
--- tests/optimizer/test_memory.py
+++ tests/optimizer/test_memory.py
@@ -6,17 +6,16 @@ class MemoryOptimizerTest(TestCase):
         optimizer = MemoryOptimizer()
         test_cases = {
             # utilization, current hard/soft limit, recommended hard/soft limit
-            ('small 90% reserved', 90.0, 80, 64, 80, 64),
-            ('small 110% reserved', 110.0, 80, 64, 80, 64),
-            ('small 113% reserved (under)', 113.0, 80, 64, 128, 96),
-            ('small 75% reserved (over)', 75.0, 80, 64, 64, 48),
-            ('tiny 25% reserved (over)', 25.0, 128, 64, 20, 16),
-            ('large 200% reserved (under)', 200.0, 1280, 1024, 2560, 2048),
+            ('16MB', 16.0, 20, 16),
+            ('48MB', 48.0, 64, 48),
+            ('57.6MB', 57.6, 80, 64),
+            ('72MB', 72.0, 128, 96),
+            ('2048MB', 2048.0, 2560, 2048),
         }
 
         for test_case in test_cases:
-            msg, utilization, current_hard_limit, current_soft_limit, expected_hard_limit, expected_soft_limit = test_case
-            peak, new_hard_limit, new_soft_limit = optimizer.recommend(utilization, current_hard_limit, current_soft_limit)
+            msg, memory, expected_hard_limit, expected_soft_limit = test_case
+            new_hard_limit, new_soft_limit = optimizer.recommend(memory)
 
             self.assertEquals(expected_soft_limit, new_soft_limit, 'soft limit %s != %s %s' % (expected_soft_limit, new_soft_limit, msg))
             self.assertEquals(expected_hard_limit, new_hard_limit, 'hard limit %s != %s %s' % (expected_hard_limit, new_hard_limit, msg))
